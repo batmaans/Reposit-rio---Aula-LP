c816b38942c09b4d92033f4bc34e0e80701c39e4
batmaans <gusttavor16@gmail.com>
2025-09-02 10:31:10 -0300

    Segundo envio

---
+++ teste.py
@@ -0,0 +1,12 @@
+pessoas = 1
+while pessoas <=4:
+    nome1 = input('Digite o nome: ')
+    print ('Olá' ,nome1, ', tudo bem?')
+
+    pessoas = pessoas + 1
+
+# contagem de números
+n1 = input('Digite um número: ')
+n2 = input('Digite outro número: ')
+n3 = n1 + n2
+print('A soma dos números é: ', n3)
